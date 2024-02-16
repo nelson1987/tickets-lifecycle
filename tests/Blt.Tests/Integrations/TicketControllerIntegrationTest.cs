9a2e9e353bdbf9b00eba252226ed7aae1ea1c612
Nelson Neto <nelson.ash@outlook.com>
2024-02-16 19:40:17 -0300

    refactor

--- tests/Blt.Tests/Integrations/TicketControllerIntegrationTest.cs
+++ tests/Blt.Tests/Integrations/TicketControllerIntegrationTest.cs
@@ -21,6 +21,17 @@ public class TicketControllerIntegrationTest : BaseIntegrationTest
         await CheckIfNewTicketWasBuyed(evento, documento);
     }
 
+    [Fact]
+    public async Task BuyTicket_Buy_Fail_Duplicate_Key_IntegrationTest()
+    {
+        var evento = "Futebol";
+        var documento = "12345678901";
+        var reserved = await BuyNewTicket(evento, documento);
+        Assert.True(reserved);
+        var reserved2 = await BuyDuplicateTicket(evento, documento);
+        Assert.True(reserved2);
+    }
+
     private async Task<bool> BuyNewTicket(string @event, string document)
     {
         var command = new BuyTicketCommand()
@@ -35,6 +46,20 @@ public class TicketControllerIntegrationTest : BaseIntegrationTest
 
         return (int)response.StatusCode == 201;
     }
+    private async Task<bool> BuyDuplicateTicket(string @event, string document)
+    {
+        var command = new BuyTicketCommand()
+        {
+            Document = document,
+            Event = @event
+        };
+
+        var response = await Client.PostAsync("/ticket", new StringContent(command.ToJson(), Encoding.UTF8, "application/json"));
+
+        Assert.Equal(400, (int)response.StatusCode);
+
+        return (int)response.StatusCode == 400;
+    }
 
     private async Task CheckIfNewTicketWasBuyed(string @event, string document)
     {
@@ -42,6 +67,5 @@ public class TicketControllerIntegrationTest : BaseIntegrationTest
 
         Assert.Equal(200, (int)response.StatusCode);
 
-        //return (int)response.StatusCode == 201;
     }
 }
