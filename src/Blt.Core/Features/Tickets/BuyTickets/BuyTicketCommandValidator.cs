3babe8014433aa81e91ccb44b09205a538aae7b4
Nelson Neto <nelson.ash@outlook.com>
2024-02-17 07:15:29 -0300

    Add Fluent Validation

---
+++ src/Blt.Core/Features/Tickets/BuyTickets/BuyTicketCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Blt.Core.Features.Tickets.BuyTickets;
+public class BuyTicketCommandValidator : AbstractValidator<BuyTicketCommand>
+{
+    public BuyTicketCommandValidator()
+    {
+        RuleFor(x => x.Event).NotNull().NotEmpty();
+        RuleFor(x => x.Document).NotNull().NotEmpty();
+    }
+}
---
+++ tests/Blt.Tests/Units/BuyTicketCommandValidatorTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Blt.Core.Features.Tickets.BuyTickets;
+using FluentValidation;
+using FluentValidation.TestHelper;
+
+namespace Blt.Tests.Units;
+public class BuyTicketCommandValidatorTests
+{
+    private readonly IFixture _fixture = new Fixture().Customize(new AutoMoqCustomization());
+    private readonly IValidator<BuyTicketCommand> _validator;
+    private readonly BuyTicketCommand _command;
+
+    public BuyTicketCommandValidatorTests()
+    {
+        _command = _fixture.Build<BuyTicketCommand>()
+            .Create();
+        _validator = _fixture.Create<BuyTicketCommandValidator>();
+    }
+
+    [Fact]
+    public void Given_a_valid_command_when_all_fields_are_valid_should_pass_validation()
+        => _validator
+            .TestValidate(_command)
+            .ShouldNotHaveAnyValidationErrors();
+
+    [Fact]
+    public void Given_a_request_with_invalid_event_should_fail_validation()
+        => _validator
+            .TestValidate(_command with { Event = string.Empty })
+            .ShouldHaveValidationErrorFor(x => x.Event)
+            .Only();
+    [Fact]
+    public void Given_a_request_with_invalid_document_should_fail_validation()
+        => _validator
+            .TestValidate(_command with { Document = string.Empty })
+            .ShouldHaveValidationErrorFor(x => x.Document)
+            .Only();
+}
